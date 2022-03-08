9afb945dbd6bac41c0839ba8cf35d95ecd35f397
Reymon Reyes <reymon.reyes@ttmsystems.co.nz>
2022-03-08 12:30:14 +0800

    [WIP] Create/edit product page.

--- Northwind.Blazor/Models/Product.cs
+++ Northwind.Blazor/Models/Product.cs
@@ -1,10 +1,20 @@
-﻿namespace Northwind.Blazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Northwind.Blazor.Models
 {
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Code { get; set; }
-        public string Description { get; set; }
+        [Required, MaxLength(40)]
+        public string Name { get; set; } = string.Empty;
+        [MaxLength(16)]
+        public string? Code { get; set; }
+        public string? Description { get; set; }
+        public string? QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public int UnitsOnOrder { get; set; }
+        public int ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
     }
 }
---
+++ Northwind.Common/Enums/EntryMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Common.Enums
+{
+    public enum EntryMode
+    {
+        Create = 0,
+        Edit = 1
+    }
+}
