2c4865cb58ef04cd8b84425ae22b5c408a681ea2
Reymon Reyes <reymon.reyes@ttmsystems.co.nz>
2022-03-08 19:58:12 +0800

    Added mapping helper for front-end.

---
+++ Northwind.Blazor/Helpers/MapperHelper.cs
@@ -0,0 +1,18 @@
+﻿using Nelibur.ObjectMapper;
+using Northwind.Blazor.Models;
+using Northwind.Core.Dtos;
+
+namespace Northwind.Blazor.Helpers
+{
+    public static class MapperHelper
+    {
+        public static Product? ToProduct(ProductDto? productDto)
+        {
+            if (productDto is null)
+                return null;
+
+            TinyMapper.Bind<ProductDto, Product>();
+            return TinyMapper.Map<Product>(productDto);
+        }
+    }
+}
