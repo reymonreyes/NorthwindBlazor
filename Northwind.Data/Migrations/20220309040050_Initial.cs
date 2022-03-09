206f2d7bbd5dc374cdfd76b17249cdcbb19ccf0f
Reymon Reyes <reymon.reyes@ttmsystems.co.nz>
2022-03-09 13:02:24 +0800

    Use HiLo sequence for id key in product.

---
+++ Northwind.Data/Migrations/20220309040050_Initial.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Northwind.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "EntityFrameworkHiLoSequence",
+                incrementBy: 10,
+                startValue: 78);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropSequence(
+                name: "EntityFrameworkHiLoSequence");
+        }
+    }
+}
