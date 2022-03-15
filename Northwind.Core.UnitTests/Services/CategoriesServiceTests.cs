330dea5894c023a71e61e370616d11e1affa3931
Reymon Reyes <reymon.reyes@ttmsystems.co.nz>
2022-03-15 20:53:29 +0800

    Validations for adding a category.

---
+++ Northwind.Core.UnitTests/Services/CategoriesServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using Autofac.Extras.Moq;
+using Moq;
+using Northwind.Core.Dtos;
+using Northwind.Core.Interfaces.Services;
+using Northwind.Core.Interfaces.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Northwind.Core.UnitTests.Services
+{
+    public class CategoriesServiceTests
+    {
+        [Fact]
+        public async Task Create_ShouldThrowExceptionIfInputIsNull()
+        {
+            var mock = AutoMock.GetLoose();
+            ICategoriesService categoriesService = mock.Create<CategoriesService>();
+            await Assert.ThrowsAsync<ArgumentNullException>(() => categoriesService.Create(null));
+        }
+
+        [Fact]
+        public void Create_ShouldUseValidatorForInputValidation()
+        {
+            var mock = AutoMock.GetLoose();
+            var validatorMock = mock.Mock<ICategoryValidator>();
+            validatorMock.Setup(x => x.Validate(It.IsAny<CategoryDto>())).Returns(new List<ServiceMessageResult>
+            {
+                new ServiceMessageResult{
+                    MessageType = Enums.ServiceMessageType.Error,
+                    Message = new KeyValuePair<string, string>("Name", "Required")
+                }
+            }).Verifiable();
+            var categoryDto = new CategoryDto { Name = "Category A" };
+            ICategoriesService categoriesService = mock.Create<CategoriesService>();
+            categoriesService.Create(categoryDto);
+            validatorMock.Verify(x => x.Validate(categoryDto), Times.Once);
+        }
+
+        [Fact]
+        public async Task Create_ShouldFailIfInvalidInputs()
+        {
+            var mock = AutoMock.GetLoose();
+            mock.Mock<ICategoryValidator>().Setup(x => x.Validate(It.IsAny<CategoryDto>())).Returns(new List<ServiceMessageResult>
+            {
+                new ServiceMessageResult{
+                    MessageType = Enums.ServiceMessageType.Error,
+                    Message = new KeyValuePair<string, string>("Name", "Required")
+                }
+            });
+            ICategoriesService categoriesService = mock.Create<CategoriesService>();
+            var result = await categoriesService.Create(new CategoryDto());
+            Assert.False(result.IsSuccessful);
+        }
+    }
+}
---
+++ Northwind.Core/Services/CategoriesService.cs
@@ -0,0 +1,36 @@
+﻿using Northwind.Core.Dtos;
+using Northwind.Core.Interfaces.Services;
+using Northwind.Core.Interfaces.Validators;
+
+namespace Northwind.Core
+{
+    public class CategoriesService : ICategoriesService
+    {
+        private ICategoryValidator _categoryValidator;
+        public CategoriesService(ICategoryValidator categoryValidator)
+        {
+            _categoryValidator = categoryValidator;
+        }
+        public async Task<ServiceResult> Create(CategoryDto? categoryDto)
+        {
+            if(categoryDto == null)
+                throw new ArgumentNullException("category");
+
+            var result = new ServiceResult { IsSuccessful = true, Messages = new List<ServiceMessageResult>() };
+
+            var validationResult = _categoryValidator.Validate(categoryDto);            
+            if(validationResult?.Count > 0)
+            {
+                result.IsSuccessful = false;
+                validationResult.AddRange(validationResult);
+            }
+
+            if (!result.IsSuccessful)
+                return result;
+
+
+
+            return result;
+        }
+    }
+}
