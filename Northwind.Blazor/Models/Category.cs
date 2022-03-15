9cf2f1182fdd7a6779519957c09897a663e63c5c
Reymon Reyes <reymon.reyes@ttmsystems.co.nz>
2022-03-15 13:08:28 +0800

    Create category page.

---
+++ Northwind.Blazor/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Northwind.Blazor.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required,MaxLength(15)]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public byte[]? Picture { get; set; }
+    }
+}
