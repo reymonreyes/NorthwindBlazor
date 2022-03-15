18e856903f3b62a5c1210ce6115ad2d89852c38c
Reymon Reyes <reymon.reyes@ttmsystems.co.nz>
2022-03-15 21:02:13 +0800

    Create category repository interface.

---
+++ Northwind.Core/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Northwind.Core
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public byte[]? Picture { get; set; }
+    }
+}
