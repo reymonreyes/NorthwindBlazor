faf4bdc97cdb265f64d24e24fd6e9d4bb7dd7e01
Reymon Reyes <reymon.reyes@ttmsystems.co.nz>
2022-03-20 14:25:30 +0800

    Product details page.

---
+++ Northwind.Blazor/Pages/PageBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Northwind.Blazor.Pages
+{
+    public class PageBase : ComponentBase
+    {
+        [Inject]
+        public ISnackbar Snackbar { get; set; }
+
+        public bool IsLoading { get; set; }
+        public bool HasError { get; set; }
+        public void Notify(string message, Severity severity)
+        {
+            Snackbar.Configuration.PositionClass = Defaults.Classes.Position.TopEnd;
+            Snackbar.Add(message, severity);
+        }
+    }
+}
