bd0f3d370756a4fcc0c65b7ddf7ce579eca76da3
Reymon Reyes <reymon.reyes@ttmsystems.co.nz>
2022-03-22 20:19:24 +0800

    Supplier input validator.

---
+++ Northwind.Common/Validators/SupplierValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using Northwind.Core.Dtos;
+using Northwind.Core.Enums;
+using Northwind.Core.Interfaces.Validators;
+
+namespace Northwind.Common.Validators
+{
+    public class SupplierValidator : AbstractValidator<SupplierDto>, ISupplierValidator
+    {
+        public SupplierValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(40);
+            RuleFor(x => x.ContactName).MaximumLength(30);
+            RuleFor(x => x.ContactTitle).MaximumLength(30);
+            RuleFor(x => x.Address).MaximumLength(60);
+            RuleFor(x => x.City).MaximumLength(15);
+            RuleFor(x => x.Region).MaximumLength(15);
+            RuleFor(x => x.PostalCode).MaximumLength(15);
+            RuleFor(x => x.Country).MaximumLength(15);
+            RuleFor(x => x.Phone).MaximumLength(24);
+            RuleFor(x => x.Fax).MaximumLength(24);
+            RuleFor(x => x.Email).MaximumLength(254).EmailAddress();
+        }
+
+        List<ServiceMessageResult>? Core.Interfaces.Validators.IValidator<SupplierDto>.Validate(SupplierDto supplier)
+        {
+            var validationResult = Validate(supplier);
+            return validationResult.Errors.Select(x => new ServiceMessageResult { MessageType = ServiceMessageType.Error, Message =  new KeyValuePair<string, string>(x.PropertyName, x.ErrorMessage)}).ToList();
+        }
+    }
+}
--- Northwind.Core/Dtos/SupplierDto.cs
+++ Northwind.Core/Dtos/SupplierDto.cs
@@ -9,17 +9,17 @@ namespace Northwind.Core.Dtos
     public class SupplierDto
     {
         public int Id { get; set; }
-        public string? Name { get; set; } = string.Empty;
-        public string? ContactName { get; set; } = string.Empty;
-        public string? ContactTitle { get; set; } = string.Empty;
-        public string? Address { get; set; } = string.Empty;
-        public string? City { get; set; } = string.Empty;
-        public string? Region { get; set; } = string.Empty;
-        public string? Country { get; set; } = string.Empty;
-        public string? PostalCode { get; set; } = string.Empty;
-        public string? Phone { get; set; } = string.Empty;
-        public string? Fax { get; set; } = string.Empty;
-        public string? Homepage { get; set; } = string.Empty;
-        public string? Email { get; set; } = string.Empty;
+        public string? Name { get; set; }
+        public string? ContactName { get; set; }
+        public string? ContactTitle { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Region { get; set; }
+        public string? Country { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Phone { get; set; }
+        public string? Fax { get; set; }
+        public string? Homepage { get; set; }
+        public string? Email { get; set; }
     }
 }
