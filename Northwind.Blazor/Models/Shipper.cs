4e21d5074ccaebc95f6fa81739421de5a212d577
Reymon Reyes <reymon.reyes@ttmsystems.co.nz>
2022-03-24 09:59:54 +0800

    Shippers table.

---
+++ Northwind.Blazor/Models/Shipper.cs
@@ -0,0 +1,9 @@
+﻿namespace Northwind.Blazor.Models
+{
+    public class Shipper
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+    }
+}
