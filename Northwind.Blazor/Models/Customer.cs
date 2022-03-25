8c228d01eedaa8f9216d9b96007f21ee1ce3b0b0
Reymon Reyes <reymon.reyes@ttmsystems.co.nz>
2022-03-25 15:48:34 +0800

    Customers table.

---
+++ Northwind.Blazor/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Northwind.Blazor.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(40)]
+        public string? Name { get; set; }
+        [MaxLength(30)]
+        public string? ContactName { get; set; }
+        [MaxLength(30)]
+        public string? ContactTitle { get; set; }
+        [MaxLength(60)]
+        public string? Address { get; set; }
+        [MaxLength(15)]
+        public string? City { get; set; }
+        [MaxLength(15)]
+        public string? Region { get; set; }
+        [MaxLength(15)]
+        public string? Country { get; set; }
+        [MaxLength(10)]
+        public string? PostalCode { get; set; }
+        [MaxLength(24)]
+        public string? Phone { get; set; }
+        [MaxLength(24)]
+        public string? Fax { get; set; }
+        //[EmailAddress, MaxLength(254)]
+        //public string? Email { get; set; }
+    }
+}
