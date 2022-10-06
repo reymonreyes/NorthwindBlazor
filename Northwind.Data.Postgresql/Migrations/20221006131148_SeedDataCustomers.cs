2820a30a16a8e4afa78e726b14e999d5fb274bf3
Reymon Reyes <elmoe01@gmail.com>
2022-10-06 21:20:24 +0800

    Migration for seed data of customers.

---
+++ Northwind.Data.Postgresql/Migrations/20221006131148_SeedDataCustomers.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Xml.Linq;
+
+#nullable disable
+
+namespace Northwind.Data.Postgresql.Migrations
+{
+    public partial class SeedDataCustomers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string dir = Environment.CurrentDirectory;
+            string file = Path.Combine(dir, "SeedData", "northwind.tables.customers.xml");
+            var xmlDoc = XElement.Load(file);
+            var records = xmlDoc.Elements("record");
+            if (records != null)
+            {
+                foreach (var record in records)
+                {
+                    migrationBuilder.InsertData(
+                        table: "customers",
+                        columns: new string[] { "Name", "ContactName", "ContactTitle", "Address", "City", "State", "PostalCode", "Country", "Phone", "Fax", "Website", "Email" },
+                        values: new object[] {
+                            record.Element("Name")?.Value,
+                            record.Element("ContactName")?.Value,
+                            record.Element("ContactTitle")?.Value,
+                            record.Element("Address")?.Value,
+                            record.Element("City")?.Value,
+                            record.Element("State").Value ?? null,
+                            record.Element("PostalCode")?.Value,
+                            record.Element("Country")?.Value,
+                            record.Element("Phone")?.Value,
+                            record.Element("Fax").Value ?? null,
+                            record.Element("Website")?.Value,
+                            record.Element("Email")?.Value,
+                        }
+                    );
+                }
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
