610fc2a60a3a62258b59cab28ff4b740eb4ecce7
Reymon Reyes <elmoe01@gmail.com>
2023-08-15 17:22:15 +0800

    Unit test for Create method of purchase order service.

--- Northwind.Core.UnitTests/Services/PurchaseOrdersTests.cs
+++ Northwind.Core.UnitTests/Services/PurchaseOrdersTests.cs
@@ -4,6 +4,7 @@ using Northwind.Common.Validators;
 using Northwind.Core.Dtos;
 using Northwind.Core.Entities;
 using Northwind.Core.Exceptions;
+using Northwind.Core.Interfaces.Repositories;
 using Northwind.Core.Interfaces.Services;
 using Northwind.Core.Interfaces.Validators;
 using Northwind.Core.Services;
@@ -19,16 +20,16 @@ namespace Northwind.Core.UnitTests.Services
     public class PurchaseOrdersTests
     {        
         [Fact]
-        public void Create_ShouldThrowNullExceptionForNullParameter()
+        public async Task Create_ShouldThrowNullExceptionForNullParameter()
         {
             var mock = AutoMock.GetLoose();
             IPurchaseOrderValidator validator = mock.Create<PurchaseOrderValidator>();
             IPurchaseOrdersService service = mock.Create<PurchaseOrdersService>();
-            Assert.Throws<ArgumentNullException>(() => service.Create(null));
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await service.Create(null));
         }
 
         [Fact]
-        public void Create_ShouldThrowValidationExceptionForInvalidFields()
+        public async Task Create_ShouldThrowValidationExceptionForInvalidFields()
         {
             var poDto = new PurchaseOrderDto { SupplierId = 1, OrderItems = new List<OrderItemDto> { new OrderItemDto { ProductId = 1, Quantity = 1 } } };            
             var mock = AutoMock.GetLoose();
@@ -37,7 +38,7 @@ namespace Northwind.Core.UnitTests.Services
                 new ServiceMessageResult { Message = new KeyValuePair<string, string>("SupplierId", "SupplierId is required"), MessageType = Enums.ServiceMessageType.Error }
             });
 ;            IPurchaseOrdersService service = mock.Create<PurchaseOrdersService>();
-            Assert.Throws<ValidationFailedException>(() => service.Create(poDto));
+            await Assert.ThrowsAsync<ValidationFailedException>(async () => await service.Create(poDto));
         }
 
         [Fact]
@@ -56,7 +57,7 @@ namespace Northwind.Core.UnitTests.Services
         }
 
         [Fact]
-        public void Create_PurchaseOrderMustContainAnItem()
+        public async Task Create_PurchaseOrderMustContainAnItem()
         {
             var purchaseOrder = new PurchaseOrderDto { SupplierId = 1 };
             var mock = AutoMock.GetLoose();
@@ -64,12 +65,12 @@ namespace Northwind.Core.UnitTests.Services
             validator.Setup(x => x.Validate(purchaseOrder)).Returns(new List<ServiceMessageResult>());
             var service = mock.Create<PurchaseOrdersService>();
 
-            var exception = Assert.Throws<ValidationFailedException>(() => service.Create(purchaseOrder));
+            var exception = await Assert.ThrowsAsync<ValidationFailedException>(async () => await service.Create(purchaseOrder));
             Assert.Contains(exception.ValidationErrors, x => x.Message.Value.Contains("Order Items are required"));
         }
 
         [Fact]
-        public void Create_PurchaseOrderMustThrowExceptionIfItemQuantityIsLessThanOne()
+        public async Task Create_PurchaseOrderMustThrowExceptionIfItemQuantityIsLessThanOne()
         {
             var purchaseOrder = new PurchaseOrderDto { SupplierId = 1, OrderItems = new List<OrderItemDto> { new OrderItemDto { ProductId = 1, Quantity = 0 } } };
             var mock = AutoMock.GetLoose();
@@ -77,12 +78,12 @@ namespace Northwind.Core.UnitTests.Services
             validator.Setup(x => x.Validate(purchaseOrder)).Returns(new List<ServiceMessageResult>());
             var service = mock.Create<PurchaseOrdersService>();
 
-            var exception = Assert.Throws<ValidationFailedException>(() => service.Create(purchaseOrder));
+            var exception = await Assert.ThrowsAsync<ValidationFailedException>(async () => await service.Create(purchaseOrder));
             Assert.Contains(exception.ValidationErrors, x => x.Message.Value.Contains("Order Item quantity must be greater than 0"));            
         }
 
         [Fact]
-        public void Create_PurchaseOrderMustThrowExceptionIfItemUnitCostIsLessThanOne()
+        public async Task Create_PurchaseOrderMustThrowExceptionIfItemUnitCostIsLessThanZero()
         {
             var purchaseOrder = new PurchaseOrderDto { SupplierId = 1, OrderItems = new List<OrderItemDto> { new OrderItemDto { ProductId = 1, Quantity = 1, UnitCost = 0 } } };
             var mock = AutoMock.GetLoose();
@@ -90,8 +91,25 @@ namespace Northwind.Core.UnitTests.Services
             validator.Setup(x => x.Validate(purchaseOrder)).Returns(new List<ServiceMessageResult>());
             var service = mock.Create<PurchaseOrdersService>();
 
-            var exception = Assert.Throws<ValidationFailedException>(() => service.Create(purchaseOrder));
+            var exception = await Assert.ThrowsAsync<ValidationFailedException>(async () => { await service.Create(purchaseOrder); });
             Assert.Contains(exception.ValidationErrors, x => x.Message.Value.Contains("Order Item unit cost must be greater than 0"));
+        }
+
+        [Fact]
+        public async Task Create_PurchaseOrderMustReturnId()
+        {
+            var purchaseOrderDto = new PurchaseOrderDto { SupplierId = 1, OrderItems = new List<OrderItemDto> { new OrderItemDto { ProductId = 1, Quantity = 1, UnitCost = 1 } } };
+            var mock = AutoMock.GetLoose();
+            var validator = mock.Mock<IPurchaseOrderValidator>();
+            validator.Setup(x => x.Validate(purchaseOrderDto)).Returns(new List<ServiceMessageResult>());
+            var repo = mock.Mock<IPurchaseOrdersRepository>();
+            repo.Setup(x => x.Create(It.IsAny<PurchaseOrder>())).ReturnsAsync(1);
+
+            var service = mock.Create<PurchaseOrdersService>();
+
+            int result = await service.Create(purchaseOrderDto);
+
+            Assert.True(result > 0);
         }
     }
 }
