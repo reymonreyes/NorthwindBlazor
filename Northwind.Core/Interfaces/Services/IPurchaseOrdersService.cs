97f642324faa0cd087a31f18e0404d5907b9e413
Reymon Reyes <elmoe01@gmail.com>
2023-09-08 05:55:06 +0800

    Method for sending the purchase order to supplier.

--- Northwind.Core/Interfaces/Services/IPurchaseOrdersService.cs
+++ Northwind.Core/Interfaces/Services/IPurchaseOrdersService.cs
@@ -12,6 +12,7 @@ namespace Northwind.Core.Interfaces.Services
         Task<ServiceMessageResult> ApproveAsync(int id);
         Task<ServiceMessageResult> CancelAsync(int id);
         Task<ServiceResult> Create(PurchaseOrderDto purchaseOrder);
+        Task EmailPdfToSupplier(int purchaseOrderId);
         Task<string> GeneratePdfDocument(int id);
         Task<ServiceMessageResult> SubmitAsync(int id);
         Task<ServiceResult> UpdateAsync(int id, PurchaseOrderDto purchaseOrderDto);
