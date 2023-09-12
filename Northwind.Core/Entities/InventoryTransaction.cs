4ea9fb9fc04a947f109d4e4541477e68bd10cefb
Reymon Reyes <elmoe01@gmail.com>
2023-09-12 20:20:55 +0800

    InventoryTransaction entity.

---
+++ Northwind.Core/Entities/InventoryTransaction.cs
@@ -0,0 +1,21 @@
+﻿using Northwind.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Core.Entities
+{
+    public class InventoryTransaction
+    {
+        public int Id { get; set; }
+        public  InventoryTransactionType TransactionType { get; set; }
+        public DateTime Created { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public int PurchaseOrderId { get; set; }
+        //public int CustomerOrderId { get; set; }
+        public string Comments { get; set; }
+    }
+}
