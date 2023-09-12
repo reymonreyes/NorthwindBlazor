39860d427a365c1058987c84e689d48c49245a3c
Reymon Reyes <elmoe01@gmail.com>
2023-09-12 20:21:09 +0800

    InventoryTransactionType enum.

---
+++ Northwind.Core/Enums/InventoryTransactionType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Core.Enums
+{
+    public enum InventoryTransactionType
+    {
+        Purchased = 10,
+        Sold = 20,
+        OnHold = 30,
+        Waste = 40
+    }
+}
