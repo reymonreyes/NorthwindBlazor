5f87b3465db0416903c2ea86062170b0f81fe1c7
Reymon Reyes <elmoe01@gmail.com>
2023-09-13 19:09:36 +0800

    Migration for changing column data type of Method property.

---
+++ Northwind.Data.Postgresql/Migrations/20230913105940_ChangePaymentMethodTypeToEnum.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Northwind.Data.Postgresql.Migrations
+{
+    public partial class ChangePaymentMethodTypeToEnum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Payment_Method",
+                table: "purchaseorders"
+            );
+
+            migrationBuilder.AddColumn<int>(
+                name: "Payment_Method",
+                table: "purchaseorders",
+                type: "smallint"
+            );            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Payment_Method",
+                table: "purchaseorders",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
