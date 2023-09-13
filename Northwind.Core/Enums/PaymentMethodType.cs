012b161005d4a15e9d71d893bd6bf7acc3bd6965
Reymon Reyes <elmoe01@gmail.com>
2023-09-13 19:07:58 +0800

    Enum for payment method type.

---
+++ Northwind.Core/Enums/PaymentMethodType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Core.Enums
+{
+    public enum PaymentMethodType
+    {
+        Cash = 10,
+        Check = 20,
+        CreditCard = 30
+    }
+}
