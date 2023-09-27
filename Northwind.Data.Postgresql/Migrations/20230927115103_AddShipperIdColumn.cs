862feddf6e2085f29dab13b53ffedfcb89700bf5
Reymon Reyes <elmoe01@gmail.com>
2023-09-27 19:56:08 +0800

    Migration for the ShipperId property.

---
+++ Northwind.Data.Postgresql/Migrations/20230927115103_AddShipperIdColumn.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Northwind.Data.Postgresql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShipperIdColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int?>(
+                name: "ShipperId",
+                table: "customerorders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customerorders_ShipperId",
+                table: "customerorders",
+                column: "ShipperId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_customerorders_shippers_ShipperId",
+                table: "customerorders",
+                column: "ShipperId",
+                principalTable: "shippers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_customerorders_shippers_ShipperId",
+                table: "customerorders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_customerorders_ShipperId",
+                table: "customerorders");
+
+            migrationBuilder.DropColumn(
+                name: "ShipperId",
+                table: "customerorders");
+        }
+    }
+}
