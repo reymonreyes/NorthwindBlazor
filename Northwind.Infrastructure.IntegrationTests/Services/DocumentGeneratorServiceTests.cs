9a8c93b5328503be539e6efb13e57215cbe07c19
Reymon Reyes <elmoe01@gmail.com>
2023-10-01 09:15:14 +0800

    Unit tests for creating a pdf invoice method.

--- Northwind.Infrastructure.IntegrationTests/Services/DocumentGeneratorServiceTests.cs
+++ Northwind.Infrastructure.IntegrationTests/Services/DocumentGeneratorServiceTests.cs
@@ -27,16 +27,15 @@ namespace Northwind.Infrastructure.IntegrationTests.Services
         {
             var mock = AutoMock.GetLoose();
             var service = mock.Create<DocumentGeneratorService>();
-            _fixture.Model.LineItems = null;
-            string filename = service.CreatePurchaseOrderPdf(_fixture.Model);
+            string filename = service.CreatePurchaseOrderPdf(_fixture.PurchaseOrderModel);
             //expect filename is in correct format
-            Assert.Contains(_fixture.Filename, filename);
+            Assert.Contains(_fixture.PurchaseOrderFilename, filename);
 
             //expect that content from inside of pdf matches the input data
             var pdfContent = _fixture.ReadPdfFile(filename);
 
             //just a simple check for company info, it would be too exhaustive if we check for each of the data in the pdf
-            Assert.Contains(_fixture.Model.Company.Name, pdfContent);
+            Assert.Contains(_fixture.PurchaseOrderModel.Company.Name, pdfContent);
         }
 
         [Fact]
@@ -46,12 +45,43 @@ namespace Northwind.Infrastructure.IntegrationTests.Services
             var service = mock.Create<DocumentGeneratorService>();
 
             Assert.Throws<ArgumentNullException>(() => service.CreatePurchaseOrderPdf(null));
-        }        
+        }
+
+        [Fact]
+        public void CreateInvoicePdf_ShouldThrowExceptionIfParameterIsNull()
+        {
+            var mock = AutoMock.GetLoose();
+            var service = mock.Create<DocumentGeneratorService>();
+
+            Assert.Throws<ArgumentNullException>(() => service.CreateInvoicePdf(null));
+        }
+
+        [Fact]
+        public void CreateInvoicePdf_ShouldCreatePdfOnCompleteData()
+        {
+            var mock = AutoMock.GetLoose();
+            var service = mock.Create<DocumentGeneratorService>();
+            string filename = service.CreateInvoicePdf(_fixture.InvoiceModel);
+            //expect filename is in correct format
+            Assert.Contains(_fixture.InvoiceFilename, filename);
+
+            //expect that content from inside of pdf matches the input data
+            var pdfContent = _fixture.ReadPdfFile(filename);
+
+            //just a simple check for company info, it would be too exhaustive if we check for each of the data in the pdf
+            Assert.Contains(_fixture.InvoiceModel.Company.Name, pdfContent);
+        }
     }
 
     public class DocumentGeneratorServiceFixture
     {
         public DocumentGeneratorServiceFixture()
+        {
+            InitializePurchaseOrderModel();
+            InitializeInvoiceModel();
+        }
+
+        private void InitializePurchaseOrderModel()
         {
             var model = new DocumentDtos.PurchaseOrderDto();
             model.Company = new BasicCompanyInformation { Name = "Northwind Traders", Address = new Address { Street = "123 First St.", City = "Scranton", State = "PA", PostalCode = "10001", Phone = "(999)123-4567" } };
@@ -78,12 +108,46 @@ namespace Northwind.Infrastructure.IntegrationTests.Services
                 });
             }
 
-            Model = model;
-            Filename = $"purchase-order-{model.PONumber}";            
+            PurchaseOrderModel = model;
+            PurchaseOrderFilename = $"purchase-order-{model.PONumber}";
         }
 
-        public DocumentDtos.PurchaseOrderDto Model { get; private set; }
-        public string Filename { get; private set; }
+        private void InitializeInvoiceModel()
+        {
+            var model = new DocumentDtos.Invoice();
+            model.Company = new BasicCompanyInformation { Name = "Northwind Traders", Address = new Address { Street = "123 First St.", City = "Scranton", State = "PA", PostalCode = "10001", Phone = "(999)123-4567" } };
+            model.ShipTo = new BasicCompanyInformation { Name = "Acme Distributors", Address = new Address { Street = "22nd St.", City = "New York", State = "NY", PostalCode = "15001", Phone = "(123) 456-7890" } };
+            model.BillTo = new BasicCompanyInformation { Name = "Northwind Traders", Address = new Address { Street = "123 First St.", City = "Scranton", State = "PA", PostalCode = "10001", Phone = "(999)123-4567" } };
+            model.InvoiceNumber = "12345";
+            model.Date = DateTime.Now;
+            model.DueDate = DateTime.Now.AddDays(2);
+            model.Subtotal = 1234m;
+            model.Tax = 23.00m;
+            model.ShippingCost = 8.74m;
+            model.Total = 1500m;
+            model.PreparedBy = "Alfred Doe";
+            model.ApprovedBy = "Bruce Wayne";
+            model.Items = new List<LineItem>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                model.Items.Add(new LineItem
+                {
+                    ItemDescription = Placeholders.Name(),
+                    Qty = int.Parse(Placeholders.Integer()),
+                    UnitPrice = decimal.Parse(Placeholders.Decimal()) * 100,
+                    Total = decimal.Parse(Placeholders.Decimal()) * 100
+                });
+            }
+
+            InvoiceModel = model;
+            InvoiceFilename = $"invoice-{model.InvoiceNumber}.pdf";
+        }
+
+        public DocumentDtos.PurchaseOrderDto PurchaseOrderModel { get; private set; }
+        public DocumentDtos.Invoice InvoiceModel { get; private set; }
+        public string PurchaseOrderFilename { get; private set; }
+        public string InvoiceFilename { get; private set; }
 
         public string ReadPdfFile(string filename)
         {
