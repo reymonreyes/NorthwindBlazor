d5b7385f7de298ebfce806c423985d54d60122c5
Reymon Reyes <elmoe01@gmail.com>
2023-10-05 17:43:56 +0800

    Value object class for shipping information.

---
+++ Northwind.Core/ValueObjects/ShippingInformation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Core.ValueObjects
+{
+    public class ShippingInformation
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
