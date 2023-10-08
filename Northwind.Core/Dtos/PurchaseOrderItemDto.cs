82836a525ddefa8af5f22cfb4f0f53979fa7f455
Reymon Reyes <elmoe01@gmail.com>
2023-10-08 19:30:48 +0800

    Dto for purchase order item.

---
+++ Northwind.Core/Dtos/PurchaseOrderItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Core.Dtos
+{
+    public class PurchaseOrderItemDto
+    {
+        public int CustomerOrderid { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
