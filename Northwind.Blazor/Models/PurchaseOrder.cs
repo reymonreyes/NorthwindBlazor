992681a6c4c635c846f4677738892a63309510f4
Reymon Reyes <elmoe01@gmail.com>
2023-10-19 17:48:16 +0800

    Purchase order model.

---
+++ Northwind.Blazor/Models/PurchaseOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace Northwind.Blazor.Models
+{
+    public class PurchaseOrder
+    {
+        public int SupplierId { get; set; }
+        public string Supplier { get; set; }
+        public string ShipTo { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public string Notes { get; set; }
+    }
+}
