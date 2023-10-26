bcf03d7e1cbde0e83ef8a2703c3b326fd097ed2b
Reymon Reyes <elmoe01@gmail.com>
2023-10-26 19:29:27 +0800

    Supplier property for purchase order.

--- Northwind.Blazor/Models/PurchaseOrder.cs
+++ Northwind.Blazor/Models/PurchaseOrder.cs
@@ -1,9 +1,10 @@
-﻿namespace Northwind.Blazor.Models
+﻿using Northwind.Core.Dtos;
+
+namespace Northwind.Blazor.Models
 {
     public class PurchaseOrder
     {
-        public int SupplierId { get; set; }
-        public string Supplier { get; set; }
+        public SupplierDto? Supplier { get; set; }
         public string ShipTo { get; set; }
         public DateTime? OrderDate { get; set; }
         public string Notes { get; set; }
