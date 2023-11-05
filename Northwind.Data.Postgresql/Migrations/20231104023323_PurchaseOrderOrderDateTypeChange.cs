2a9beb66146fa43d06a31f05330cb55588acf570
Reymon Reyes <elmoe01@gmail.com>
2023-11-05 16:12:14 +0800

    Migration for changing OrderDate type to timestamp without time zone.

---
+++ Northwind.Data.Postgresql/Migrations/20231104023323_PurchaseOrderOrderDateTypeChange.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Northwind.Data.Postgresql.Migrations
+{
+    /// <inheritdoc />
+    public partial class PurchaseOrderOrderDateTypeChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "purchaseorders",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "purchaseorders",
+                type: "timestamp",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without timezone",
+                oldNullable: true);
+        }
+    }
+}
