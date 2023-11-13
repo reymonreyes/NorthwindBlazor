c149bac50937129f1d394fa9284ee76fb5c6e002
Reymon Reyes <elmoe01@gmail.com>
2023-11-13 19:44:40 +0800

    Method for deleting a purchase order item.

--- Northwind.Core/Interfaces/Repositories/IPurchaseOrderItemsRepository.cs
+++ Northwind.Core/Interfaces/Repositories/IPurchaseOrderItemsRepository.cs
@@ -10,6 +10,7 @@ namespace Northwind.Core.Interfaces.Repositories
     public interface IPurchaseOrderItemsRepository
     {
         Task Create(PurchaseOrderItem purchaseOrderItem);
+        Task DeleteAsync(int purchaseOrderItemId);
         Task<PurchaseOrderItem?> GetAsync(int purchaseOrderItemId);
     }
 }
