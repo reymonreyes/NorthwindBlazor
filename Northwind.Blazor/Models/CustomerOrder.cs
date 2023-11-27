65d3810e2e3db29299b199503d7592b91f549c2a
Reymon Reyes <elmoe01@gmail.com>
2023-11-27 16:12:02 +0800

    View model for creating/editing a customer order.

---
+++ Northwind.Blazor/Models/CustomerOrder.cs
@@ -0,0 +1,13 @@
+﻿namespace Northwind.Blazor.Models
+{
+    public class CustomerOrder
+    {
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public int ShipperId { get; set; }
+        public string? ShipTo { get; set; }
+        public string? Notes { get; set; }
+    }
+}
