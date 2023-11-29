adc75f9457abfae8ea4905973db7ed2b241a26ab
Reymon Reyes <elmoe01@gmail.com>
2023-11-29 21:18:51 +0800

    Base class for model validation.

---
+++ Northwind.Blazor/Models/Validators/BaseValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Northwind.Blazor.Models.Validators
+{
+    public class BaseValidator<T> : AbstractValidator<T>
+    {
+        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+        {
+            var result = await ValidateAsync(ValidationContext<T>.CreateWithOptions((T)model, x => x.IncludeProperties(propertyName)));
+            if (result.IsValid)
+                return Array.Empty<string>();
+            return result.Errors.Select(e => e.ErrorMessage);
+        };
+    }
+}
