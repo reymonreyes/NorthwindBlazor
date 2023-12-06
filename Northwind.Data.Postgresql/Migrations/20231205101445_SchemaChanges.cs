f897390b399489216c4e6047a59d261116263f3c
Reymon Reyes <elmoe01@gmail.com>
2023-12-06 17:48:03 +0800

    Migration for ShipDate and DueDate properties.

---
+++ Northwind.Data.Postgresql/Migrations/20231205101445_SchemaChanges.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Northwind.Data.Postgresql.Migrations
+{
+    /// <inheritdoc />
+    public partial class SchemaChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
---
+++ Northwind.Data.Postgresql/Migrations/20231205104341_CustomerOrderDueDateShipDateColumns.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Northwind.Data.Postgresql.Migrations
+{
+    /// <inheritdoc />
+    public partial class CustomerOrderDueDateShipDateColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "customerorders",
+                type: "timestamp",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ShipDate",
+                table: "customerorders",
+                type: "timestamp",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "customerorders");
+
+            migrationBuilder.DropColumn(
+                name: "ShipDate",
+                table: "customerorders");
+        }
+    }
+}
