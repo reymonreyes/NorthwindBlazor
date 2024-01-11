383e95e1ec9ed0a2cdd5945b3ff6719e590ded64
Reymon Reyes <elmoe01@gmail.com>
2024-01-11 18:51:28 +0800

    Dto for displaying customer orders list in a summary format.

---
+++ Northwind.Core/Dtos/CustomerOrderSummaryDto.cs
@@ -0,0 +1,18 @@
+﻿using Northwind.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Core.Dtos
+{
+    public class CustomerOrderSummaryDto
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public OrderStatus Status { get; set; }
+        public decimal Total { get; set; }
+    }
+}
