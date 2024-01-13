91308876d5dcf68f000519f0a390f7e451ec9115
Reymon Reyes <elmoe01@gmail.com>
2024-01-13 09:42:41 +0800

    Changed return type.

--- Northwind.Core/Interfaces/Repositories/IPurchaseOrdersRepository.cs
+++ Northwind.Core/Interfaces/Repositories/IPurchaseOrdersRepository.cs
@@ -13,6 +13,6 @@ namespace Northwind.Core.Interfaces.Repositories
         Task CreateAsync(PurchaseOrder purchaseOrder);
         Task<PurchaseOrder?> GetAsync(int id);
         void Update(PurchaseOrder purchaseOrder);
-        Task<(int TotalRecords, IEnumerable<PurchaseOrder> Records)> GetAllAsync(int page = 1, int size = 10);
+        Task<(int TotalRecords, IEnumerable<PurchaseOrderSummaryDto> Records)> GetAllAsync(int page = 1, int size = 10);
     }
 }
