4df4dc65d3b1c0027fc92361021e53c691f98140
Reymon Reyes <elmoe01@gmail.com>
2024-01-14 19:46:07 +0800

    Added CustomerName and ShipperName properties.

--- Northwind.Core/Dtos/CustomerOrderDto.cs
+++ Northwind.Core/Dtos/CustomerOrderDto.cs
@@ -11,10 +11,12 @@ namespace Northwind.Core.Dtos
     {
         public int Id { get; set; }
         public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
         public DateTime OrderDate { get; set; }
         public DateTime? DueDate { get; set; }
         public DateTime? ShippedDate { get; set; }
         public int? ShipperId { get; set; }
+        public string ShipperName { get; set; }
         public string Notes { get; set; }
         public DateTime? ShipDate { get; set; }
         public OrderStatus Status { get; set; }
