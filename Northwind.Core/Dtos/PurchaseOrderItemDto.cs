6782b1eef07e7e753b5434624739e6ca94588e5c
Reymon Reyes <elmoe01@gmail.com>
2024-01-15 16:05:17 +0800

    Added ProductName property.

--- Northwind.Core/Dtos/PurchaseOrderItemDto.cs
+++ Northwind.Core/Dtos/PurchaseOrderItemDto.cs
@@ -10,6 +10,7 @@ namespace Northwind.Core.Dtos
     {
         public int Id { get; set; }
         public int ProductId { get; set; }
+        public string ProductName { get; set; }
         public int Quantity { get; set; }
         public decimal? UnitPrice { get; set; }
     }
